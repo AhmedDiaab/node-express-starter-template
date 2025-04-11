1c42c8cf8f7850fb97a71f061b326bbf5f55626e
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2025-04-11 15:12:29 +0200

    feat(): add `JwtAuthStrategy`

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { env } from "@/utils";
+import { BaseAuthStrategy } from "@lib/auth";
+import { ExtractJwt, Strategy as JwtStrategy, StrategyOptionsWithRequest } from 'passport-jwt';
+
+export default class JwtAuthStrategy extends BaseAuthStrategy<JwtStrategy, StrategyOptionsWithRequest> {
+    name = 'jwt';
+    StrategyClass = JwtStrategy;
+
+    getOptions(): StrategyOptionsWithRequest | Partial<StrategyOptionsWithRequest> {
+        return {
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: env.jwtSecret,
+        };
+    }
+
+    async verify(payload: any, done: (error: any, user?: any) => void) {
+        try {
+            if (payload?.id) {
+                return done(null, payload);
+            }
+            return done(null, false);
+        } catch (error) {
+            return done(error);
+        }
+    }
+}
